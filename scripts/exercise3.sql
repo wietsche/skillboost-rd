15f8ae4f08ebd641b4053a2e20607be16e207388
Wietsche Calitz <wietsche@users.noreply.github.com>
2025-10-28 10:58:02 +0000

    fix

--- scripts/exercise3.sql
+++ scripts/exercise3.sql
@@ -3,13 +3,13 @@
 .headers on
 
 DROP INDEX IF EXISTS idx_inventory_sku;
--- CREATE INDEX idx_inventory_sku ON inventory(sku);
+--CREATE INDEX idx_inventory_sku ON inventory(sku);
 
 EXPLAIN QUERY PLAN
-SELECT COUNT(*) AS count_without_index FROM inventory WHERE sku = 'TVH-742472';
+SELECT COUNT(*) FROM inventory WHERE sku = 'TVH-742472';
 
 .timer on
-SELECT COUNT(*) AS count_without_index FROM inventory WHERE sku = 'TVH-742472';
+SELECT COUNT(*) FROM inventory WHERE sku = 'TVH-742472';
 .timer off
 
 
